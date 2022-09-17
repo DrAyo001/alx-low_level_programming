0a24ece003b7601071296f2e9201058a27aaff1c
Yusuf Babatunde Sodiq <asodiq001@gmail.com>
2022-09-17 11:57:51 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,20 @@
 #include "main.h"
 
 /**
- * print_alphabet - function to print abc
- * Return: 0
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line
  */
 
 void print_alphabet(void)
 {
-	char c;
+	char ch;
 
-	for (c = 'a'; c <= 'z'; c++)
-		{
-			_putchar(c);
-		}
-_putchar('\n');
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+
+	_putchar('\n');
 }
