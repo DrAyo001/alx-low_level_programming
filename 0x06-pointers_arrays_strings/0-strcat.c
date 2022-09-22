9641f04a2cd46562ec06ed6f8cdf357b8901300c
Yusuf Babatunde Sodiq <asodiq001@gmail.com>
2022-09-22 05:42:26 -0700

    Task 1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,34 +12,13 @@
 char *_strcat(char *dest, char *src)
 {
 	
-	int i, j;
-
-	i = 0;
-	j = 0;
+	int index = 0, dest_len = 0;
 	
-	while (dest[i] != '\0')
-		i++;
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		j++;
-		i++;
-	}
+	while (dest[index++])
+		dest_len++;
 
-	dest[i] = '\0';
+	for (index = 0; src[index]; index ++)
+		dest[dest_len++] = src[index];
 
 	return (dest);
-
-
-
-
-	//int index = 0, dest_len = 0;
-	
-	//while (dest[index++])
-		//dest_len++;
-
-	//for (index = 0; src[index]; index ++)
-		//dest[dest_len++] = src[index];
-
-	//return (dest);
 }
--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,14 +8,16 @@
  * Return: returns new concatenated string
  */
 
-char *strncat(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
+
 	int index = 0, dest_len = 0;
 
 	while (dest[index++])
 		dest_len++;
+
 	for (index = 0; src[indext] && index < n; index++)
 		dest[dest_len++] = src[index];
-
-	retrun (dest);
+	
+	return (dest);
 }
