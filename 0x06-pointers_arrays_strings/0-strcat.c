fa05e6e76a8fb6ea3cc7b40019bc206eb4cdb569
Yusuf Babatunde Sodiq <asodiq001@gmail.com>
2022-09-22 07:41:51 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 /**
  * _strcat - cancatenates the string pointed to by @src to
  * the end of th string pointed to by @dest
@@ -10,15 +11,14 @@
  */
 
 char *_strcat(char *dest, char *src)
+
 {
-	
+
 	int index = 0, dest_len = 0;
-	
 	while (dest[index++])
 		dest_len++;
-
-	for (index = 0; src[index]; index ++)
+	
+	for (index = 0; src[index]; index++)
 		dest[dest_len++] = src[index];
-
 	return (dest);
 }
