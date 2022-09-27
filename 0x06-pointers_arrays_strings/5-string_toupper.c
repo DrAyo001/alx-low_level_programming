b3be8cd5bef41c3e4b20a841a379d8fafb376835
Yusuf Babatunde Sodiq <asodiq001@gmail.com>
2022-09-27 05:20:46 -0700

    folder

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
- * cap_string - capitalizes all words of a string
- * @s: input string.
+ * string_toupper - changes all lowercase letters
+ * of a string to uppercase
+ * @str: input string.
  * Return: the pointer to dest.
  */
-
 char *string_toupper(char *str)
 {
-
 	int i = 0;
+
 	while (str[i] != '\0')
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
 			str[i] -= 32;
 		i++;
 	}
-	
 	return (str);
 }
