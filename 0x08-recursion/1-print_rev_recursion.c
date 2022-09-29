8f12f4d2c9b04bca9f20b9c0b92deb91acc83855
Yusuf Babatunde Sodiq <asodiq001@gmail.com>
2022-09-29 09:08:51 -0700

    update

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,11 +8,11 @@
  * Return: void
  */ 
 
-void _print_reve_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
-        if (*s != '\0')
-        {
-                _print_rev_recursion(s + 1);
-                _putchar(*s);
-        }
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
