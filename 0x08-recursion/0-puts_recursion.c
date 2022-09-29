c138aa72c7c03f4d238d137050d82ce8a99fc85e
Yusuf Babatunde Sodiq <asodiq001@gmail.com>
2022-09-29 08:42:29 -0700

    hot fix

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,16 +7,15 @@
  *
  * Return: void
  */
-
 void _puts_recursion(char *s)
-{
-        if (*s == '\n')
-        {
-            _putchar('\n');
-        }
-        else
-        {
-            _putchar(s[0]);
-            _puts_recursion(s + 1);
-        }
+{	
+	if (*s == '\n')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
+	}
 }
