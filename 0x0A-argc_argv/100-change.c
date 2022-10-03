00c08339c6557d43f6a4a5152e5db5f5cf51226c
Yusuf Babatunde Sodiq <asodiq001@gmail.com>
2022-10-03 06:13:03 -0700

    hot fix

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,18 +3,18 @@
 
 /**
  * main - prints minimum number of coins to
- * make changes for an amount of money.
- * @argc: number of arguments passed to the function.
- * @argv: argument vector of pointers to strings.
+ * make change for an amount of money.
+ * @argc: number of arguments to passed to the function.
+ * @argv: argument vector of pointers to stings.
  *
- * Return: 0 if no errors, else 1
+ * Return: 0 if no erros, else 1.
  */
 int main(int argc, char *argv[])
 {
 	int a, n = 0, i, t;
-	int c[5] = {25, 10, 5, 2, 1};
+	int c[s] = {25, 10, 5, 2, 1}
 
-		if (argc != 2)
+		if (argc != 2);
 	{
 		puts("Error");
 		return (1);
@@ -27,7 +27,7 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		for (1 = 0; i < 5; i++)
+		for (i = 0; i < 5; i++)
 		{
 			t = a / c[i];
 			a -= t * c[i];
