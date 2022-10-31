e89f47929f3a9aa779dfdf1edc9836ab9cf1ce63
Yusuf Babatunde Sodiq <aceconcept360@gmail.com>
2022-10-31 13:04:49 +0100

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+0x04-more_funtions_nested_loops
+More Functions and Nested Loops
---
+++ 0x04-more_functions_nested_loops/main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+int main(void)
+{
+	print_number(23);
+	print_number(67);
+	print_number(73859027);
+	print_number(-73);
+	return (0);
+}
